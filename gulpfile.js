d16c931e48755c993eca264dbde36cf9d578ed3a
Александр Гусев <mafhicon@gmail.com>
2017-12-11 13:43:15 +0300

    initial: index + index-leal + textpage + responsive

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp 	 = require('gulp'),
+	sass 	 = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rigger  = require('gulp-rigger'),
+	browserSync  = require('browser-sync'),
+	wait  = require('gulp-wait'),
+	del = require('del');
+
+var babel = require('gulp-babel');
+
+var gulpautoprefixer = require('gulp-autoprefixer');
+
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('clean', function() {
+	del.sync('build');
+});
+
+gulp.task('sass', function(){
+	var postcss      = require('gulp-postcss');
+	var autoprefixer = require('autoprefixer');
+
+	gulp.src('src/sass/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({indentType: 'tab', indentWidth: '1', outputStyle: 'expanded'}).on('error', sass.logError))
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest('src/temp'));
+		// .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('autoprefixer', function () {
+	gulp.src('src/temp/**/*.css')
+		.pipe(postcss( [ autoprefixer() ] ))
+		.pipe(gulp.dest('build/css'))
+		// .pipe(wait(400))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: 'build'
+		},
+		notify: false
+	});
+});
+
+gulp.task('html', function() {
+	gulp.src('src/**/*.html')
+		.pipe(rigger())
+		.pipe(gulp.dest('build/'))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js', function() {
+	gulp.src('src/js/**/*')
+		.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('default', ['clean', 'browser-sync', 'sass', 'autoprefixer', 'html', 'js'], function(){
+
+	var buildFonts = gulp.src('src/fonts/**/*')
+		.pipe(gulp.dest('build/fonts'));
+
+	var buildImages = gulp.src('src/i/**/*')
+		.pipe(gulp.dest('build/i'));
+
+	var buildUserimg = gulp.src('src/userimg/**/*')
+		.pipe(gulp.dest('build/userimg'));
+
+	var copyHtml = gulp.src('src/*.html')
+		.pipe(gulp.dest('build'));
+
+	var copyJs = gulp.src('src/js/**/*.js')
+		.pipe(gulp.dest('build/js'));
+
+	var buildHtml = gulp.src('src/**/*.html')
+		.pipe(rigger())
+		.pipe(gulp.dest('build/'));
+
+	gulp.watch('src/sass/**/*.scss', ['sass']);
+	gulp.watch('src/temp/**/*.css', ['autoprefixer']);
+	gulp.watch('src/**/*.html', ['html']);
+	gulp.watch('src/js/**/*.js', ['js']).on('change', browserSync.reload);
+});
